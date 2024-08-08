2214eedf989e8b7b8daceb5cdab5d0f9143d8470
Miri <mirino.otsubo@outlook.com>
2024-08-09 05:06:14 +0900

    make slider arrows and dots

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
-// #arrow-upをクリックした際の設定
+// Slider
+$('.slider').on('breakpoint', function(breakpoint) {
+    // デバッグ用
+    console.log(breakpoint);
+});
+
+$('.slider').slick({
+    respondTo: 'slider',
+    autoplay: true,
+    arrows: true,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    dots: true,
+    dotsClass: 'slide-dots',
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    centerMode: true,
+    centerPadding: '20%',
+    responsive: [{
+        breakpoint: 1000,
+        settings: {
+            centerMode: false,
+            centerPadding: '50px',
+        }
+    }, {
+        breakpoint: null,
+        settings: {
+            centerMode: true,
+            centerPadding: '20%',
+        }
+    }]
+});
+
+// ページのトップへスクロール
 $('#arrow-up').click(function () {
     $('body, html').animate({
-        scrollTop: 0    //ページトップまでスクロール
-    }, 500);    //ページトップスクロールの速さ。数字が大きいほど遅くなる
-    return false;   //リンク自体の無効化
+        scrollTop: 0
+    }, 500); //ページトップスクロールの速さ
+    return false; //リンク自体の無効化
 });
