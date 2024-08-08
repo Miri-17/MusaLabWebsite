1c84f72d5f81fdb1234cecb8bb13511dfff9da80
Miri <mirino.otsubo@outlook.com>
2024-08-08 18:36:32 +0900

    modify footer

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// #arrow-upをクリックした際の設定
+$('#arrow-up').click(function () {
+    $('body, html').animate({
+        scrollTop: 0    //ページトップまでスクロール
+    }, 500);    //ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;   //リンク自体の無効化
+});
