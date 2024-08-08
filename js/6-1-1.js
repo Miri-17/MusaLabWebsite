cfe6a17c9ea030d487bb0757b138bd2a0f2df13d
Miri <mirino.otsubo@outlook.com>
2024-08-09 02:14:48 +0900

    add slider

--- js/6-1-1.js
+++ js/6-1-1.js
@@ -1,38 +1,30 @@
-function setSlidesToShow() {
-    if (window.innerWidth >= 1000) {
-        return 2;
-    } else {
-        return 1;
-    }
-}
+$(document).ready(function(){
+    $('.slider').on('breakpoint', function(breakpoint) {
+        // デバッグ用
+        console.log(breakpoint);
+    });
 
-$('.hero').slick({
-    autoplay: true, // 自動的に動き出すか (初期値: false)
-    dots: true,
-    slidesToShow: setSlidesToShow(),
+    $('.slider').slick({
+        respondTo: 'slider',
+        arrows: true,
+        autoplay: true,
+        dots: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        centerMode: true,
+        centerPadding: '20%',
+        responsive: [{
+            breakpoint: 1000,
+            settings: {
+                centerMode: false,
+                centerPadding: '50px',
+            }
+        }, {
+            breakpoint: null,
+            settings: {
+                centerMode: true,
+                centerPadding: '20%',
+            }
+        }]
+    });
 });
-
-$(window).resize(function() {
-    $('.hero').slick('slickSetOption', 'slidesToShow', setSlidesToShow(), true);
-});
-
-// $('.slider').slick({
-//     autoplay: true,                                 // 自動的に動き出すか (初期値: false)
-//     autoplaySpeed: 3000,                            // 次のスライドに切り替わるまでの時間
-//     speed:1000,                                     // スライドするスピード (初期値: 300)
-//     infinite: true,                                 // スライドをループさせるか (初期値: true)
-//     slidesToShow: 1,                                // 1度に表示するスライド数 (初期値: 1)
-//     slidesToScroll: 1,                              // 1回のスライドで移動するスライド数 (初期値: 1)
-//     arrows: true,                                   // 左右の矢印を付けるか
-//     prevArrow: '<div class="slick-prev"></div>',    // 「前へ」の矢印をカスタムHTMLで変更
-//     nextArrow: '<div class="slick-next"></div>',    // 「次へ」の矢印をカスタムHTMLで変更
-//     dots: true,                                     // 下部のドットナビゲーションを表示するか
-//     pauseOnFocus: false,                            // フォーカスで一時停止するか
-//     pauseOnHover: false,                            // マウスホバーで一時停止するか
-//     pauseOnDotsHover: false,                        // ドットナビゲーションをマウスホバーした時に一時停止するか
-// });
-
-// // スマホ用: スライダーをタッチしても止めずにスライドをさせたい場合
-// $('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide) {
-//     $('.slider').slick('slickPlay');
-// });
