03704a9134a25019b3043976ef8a2d67b1dff8a7
Miri <mirino.otsubo@outlook.com>
2025-01-30 17:39:03 +0900

    delete comment out

--- htmlcss/assets/js/image-slider.js
+++ htmlcss/assets/js/image-slider.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
+    // デバッグ用
     $('.slider').on('breakpoint', function(breakpoint) {
-        // デバッグ用
         console.log(breakpoint);
     });
 
@@ -29,26 +29,5 @@ $(document).ready(function(){
                 centerPadding: '20%',
             }
         }]
-        // respondTo: 'slider',
-        // arrows: true,
-        // autoplay: true,
-        // dots: true,
-        // pauseOnFocus: false,
-        // pauseOnHover: false,
-        // centerMode: true,
-        // centerPadding: '20%',
-        // responsive: [{
-        //     breakpoint: 1000,
-        //     settings: {
-        //         centerMode: false,
-        //         centerPadding: '50px',
-        //     }
-        // }, {
-        //     breakpoint: null,
-        //     settings: {
-        //         centerMode: true,
-        //         centerPadding: '20%',
-        //     }
-        // }]
     });
 });
